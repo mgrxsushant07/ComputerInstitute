36f8bfd8d645f521129780a993e20dac6d04fea4
Your Name <donho4664@gmail.com>
2025-02-25 18:57:50 +0545

    Initial commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,15 @@
+# views.py
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .forms import contactform  
+
 def index(request):
-    return render(request,'mainapp/index.html')
+    if request.method == 'POST':
+        form = contactform(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'mainapp/success_form.html')
+    else:
+        form = contactform()  
+
+    return render(request, 'mainapp/index.html', {'form': form})  
